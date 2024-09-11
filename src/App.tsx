a89a271b99f2a64deb2b56920e5f591d49ca5906
Tri Nguyen <71200950+tringuyen1086@users.noreply.github.com>
2024-09-10 17:14:39 -0700

    a1 lab - React Route

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Labs from "./Labs";
+import { HashRouter, Route, Routes, Navigate } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <h1>Welcome to Web Dev</h1>
       <h2>Tri Nguyen</h2>
-      <Labs />
+      <HashRouter>
+        <div>
+          <Routes>
+            <Route path="/" element={<Navigate to="Labs" />} />
+            <Route path="/Labs/*" element={<Labs />} />
+          </Routes>
+        </div>
+      </HashRouter>
     </div>
   );
 }
