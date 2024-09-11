244c558348ad4a0e86658a0a30b99239f8edac97
Tri Nguyen <71200950+tringuyen1086@users.noreply.github.com>
2024-09-10 17:00:23 -0700

    a1 lab

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Labs from "./Labs";
 
 function App() {
   return (
     <div>
       <h1>Welcome to Web Dev</h1>
       <h2>Tri Nguyen</h2>
+      <Labs />
     </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
---
+++ src/Labs/index.tsx
@@ -0,0 +1,11 @@
+import Lab1 from "./Lab1";
+
+export default function Labs(){
+    return (
+        <div id="wd-labs">
+            <h1>Labs</h1>
+            <Lab1 />
+        </div>
+
+    )
+}
