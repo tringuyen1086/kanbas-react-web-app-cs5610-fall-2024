cc8bca0812252ce3be011d3d9c66ead22cd4ab9a
Tri Nguyen <71200950+tringuyen1086@users.noreply.github.com>
2024-09-12 16:23:26 -0700

    a1 lab - add Image tag

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -85,6 +85,21 @@ export default function Lab1() {
                 </table>
             </div>
 
+            <div id="wd-images">
+                <h4>Image tag</h4>
+                Loading an image from the internet:
+                <br />
+                <img id="wd-starship"
+                    width="400px"
+                    src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+                />
+                <br />
+                Loading a local image:
+                <br />
+                <img id="wd-teslabot" src="images/teslabot.jpg" height="200px" />
+            </div>
+
+
             <div id="wd-forms">
                 <h4>Form Elements</h4>
                 <form id="wd-text-fields">
