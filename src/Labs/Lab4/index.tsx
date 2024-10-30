63ea08fc28ab575aedc986640a6f44044fc27fad
Tri Nguyen <71200950+tringuyen1086@users.noreply.github.com>
2024-10-29 19:04:14 -0700

    a4 - Lab4 adding more components

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -1,5 +1,18 @@
+import React from "react";
+import ReduxExamples from "./ReduxExamples";
+
+import ClickEvent from "./ClickEvent";
 import EventObject from "./EventObject";
-import PassingFunctions from "./PassingFunctions"; // import the component
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions"; 
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariable";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ArrayStateVariable from "./ArrayStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
+
 export default function Lab4() {
   // implement callback function
   function sayHello() {
@@ -9,9 +22,20 @@ export default function Lab4() {
     <div id="wd-passing-functions">
       <h3>Lab 4</h3>
 
+      <ClickEvent />
+      <PassingDataOnEvent />
       {/* pass callback function as a parameter */}
       <PassingFunctions theFunction={sayHello} /> 
       <EventObject />
+      <Counter />
+      <BooleanStateVariables />
+      <StringStateVariables />
+      <DateStateVariable />
+      <ObjectStateVariable />
+      <ArrayStateVariable />
+      <ParentStateComponent />
+      <ReduxExamples />
+
     </div>
   );
 }
