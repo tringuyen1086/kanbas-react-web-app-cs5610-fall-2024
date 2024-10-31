35a15e12a6d55af09e5b2902d8df6a4fff2a7e20
Tri Nguyen <71200950+tringuyen1086@users.noreply.github.com>
2024-10-30 18:50:28 -0700

    a4 - Kanbas - Modules - Add Module Editor Dialog to Controls

--- src/Kanbas/Courses/Modules/ModulesControls.tsx
+++ src/Kanbas/Courses/Modules/ModulesControls.tsx
@@ -8,10 +8,15 @@ export default function ModulesControls(
     (title: string) => void; addModule: () => void; }) {
 return (
     <div id="wd-modules-controls" className="text-nowrap">
-        <button id="wd-add-module-btn" className="btn btn-lg btn-danger me-1 float-end"
-            data-bs-toggle="modal" data-bs-target="#wd-add-module-dialog" >
+        <button 
+            id="wd-add-module-btn" 
+            className="btn btn-lg btn-danger me-1 float-end"
+            data-bs-toggle="modal" 
+            data-bs-target="#wd-add-module-dialog" >
+                
             <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
             Module</button>
+
         <div className="dropdown d-inline me-1 float-end">
             <button id="wd-publish-all-btn" className="btn btn-lg btn-secondary dropdown-toggle"
             type="button" data-bs-toggle="dropdown">
@@ -56,6 +61,11 @@ return (
         style={{ bottom: "1px" }}>
             Collapse All</button>
 
+        <ModuleEditor 
+            dialogTitle="Add Module" 
+            moduleName={moduleName}
+            setModuleName={setModuleName} 
+            addModule={addModule} />
 
     </div>
 )
