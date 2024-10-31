9466d4224a7828e6ebe137fea65d550474572235
Tri Nguyen <71200950+tringuyen1086@users.noreply.github.com>
2024-10-30 20:11:26 -0700

    a4 - Kanbas - Editing a Module

--- src/Kanbas/Courses/Modules/ModuleControlButtons.tsx
+++ src/Kanbas/Courses/Modules/ModuleControlButtons.tsx
@@ -2,20 +2,22 @@ import { IoEllipsisVertical } from "react-icons/io5";
 import GreenCheckmark from "./GreenCheckmark";
 import { BsPlus } from "react-icons/bs"; // Plus icon
 import { FaTrash } from "react-icons/fa";
+import { FaPencil } from "react-icons/fa6";
 
 export default function ModuleControlButtons(
-{ moduleId, deleteModule }: { moduleId: string;
-    deleteModule: (moduleId: string) => void; } 
+{ moduleId, deleteModule, editModule }: { moduleId: string;
+    deleteModule: (moduleId: string) => void; 
+    editModule: (moduleId: string) => void } 
 ) {
     return (
-    <div className="d-flex align-items-center justify-content-end gap-3">
-        <div className="float-end">
-        <FaTrash className="text-danger me-2 mb-1"
+    <div className="float-end">
+        <FaPencil onClick={() => editModule(moduleId)}
+                className="text-primary me-3" />
+        <FaTrash className="text-danger me-3 mb-1"
                     onClick={() => deleteModule(moduleId)}/>
         <GreenCheckmark />
         <BsPlus className="fs-1" />
         <IoEllipsisVertical className="fs-4" />
-        </div>
     </div>
     );
 }
--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -27,6 +27,14 @@ export default function Modules() {
     setModules(modules.filter((m) => m._id !== moduleId));
   };
 
+  const editModule = (moduleId: string) => {
+    setModules(modules.map((m) => (m._id === moduleId ?
+      { ...m, editing: true } : m)));
+  };
+  const updateModule = (module: any) => {
+    setModules(modules.map((m) => (m._id === module._id ?
+      module : m)));
+  };
 
   return (
     <div>
@@ -46,12 +54,25 @@ export default function Modules() {
             <div className="wd-title p-3 ps-2 bg-secondary text-black d-flex align-items-center">
               <BsGripVertical className="me-2 fs-3" />
               {/* Module name should take up available space */}
-              <span className="flex-grow-1 fw-bold">{module.name}</span>
+              <span className="flex-grow-1 fw-bold">
+              {!module.editing && module.name}
+              { module.editing && (
+                <input className="form-control w-50 d-inline-block"
+                      onChange={(e) => updateModule(
+                        { ...module, name: e.target.value })}
+                      onKeyDown={(e) => {
+                        if (e.key === "Enter") {
+                          updateModule({ ...module, editing: false });
+                        }
+                      }}
+                      value={module.name}/>
+              )}</span>
               
               {/* Control buttons aligned to the right */}
               <ModuleControlButtons 
                 moduleId={module._id}
-                deleteModule={deleteModule}/>
+                deleteModule={deleteModule}
+                editModule={editModule}/>
             </div>
             {module.lessons && (
               <ul className="wd-lessons list-group rounded-0">
