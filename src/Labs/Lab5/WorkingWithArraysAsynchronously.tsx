cc0e2229f09a115a10569e4fc986aa975d63bc86
Tri Nguyen <71200950+tringuyen1086@users.noreply.github.com>
2024-11-20 22:28:14 -0800

    a5 - lab5 - Handling Errors - Completed lab5

--- src/Labs/Lab5/WorkingWithArraysAsynchronously.tsx
+++ src/Labs/Lab5/WorkingWithArraysAsynchronously.tsx
@@ -2,12 +2,41 @@ import React, { useState, useEffect } from "react";
 import * as client from "./client";
 import { FaTrash } from "react-icons/fa";
 import { FaPlusCircle } from "react-icons/fa";
+import { TiDelete } from "react-icons/ti";
+import { FaPencil } from "react-icons/fa6";
+
 
 export default function WorkingWithArraysAsynchronously() {
     const [todos, setTodos] = useState<any[]>([]);
     const createTodo = async () => {
         const todos = await client.createTodo();
         setTodos(todos);
+    };
+
+    const postTodo = async () => {
+        const newTodo = await client.postTodo({ title: "New Posted Todo", completed: false, });
+        setTodos([...todos, newTodo]);
+    };
+
+    const editTodo = (todo: any) => {
+        const updatedTodos = todos.map(
+            (t) => t.id === todo.id ? { ...todo, editing: true } : t );
+        setTodos(updatedTodos);
+    };
+
+    const updateTodo = async (todo: any) => {
+        //await client.updateTodo(todo);
+        setTodos(todos.map((t) => (t.id === todo.id ? todo : t)));
+    };
+
+    const [errorMessage, setErrorMessage] = useState(null);
+    const saveTodo = async (todo: any) => {
+        try {
+            await client.updateTodo(todo);
+            setTodos(todos.map((t) => (t.id === todo.id ? todo : t)));
+        } catch (error: any) {
+            setErrorMessage(error.response.data.message);
+        }
     };
     
     const fetchTodos = async () => {
@@ -18,6 +47,18 @@ export default function WorkingWithArraysAsynchronously() {
         const updatedTodos = await client.removeTodo(todo);
         setTodos(updatedTodos);
     };
+
+    const deleteTodo = async (todo: any) => {
+        try {
+            await client.deleteTodo(todo);
+            const newTodos = todos.filter((t) => t.id !== todo.id);
+            setTodos(newTodos);
+        } catch (error: any) {
+            console.log(error);
+            setErrorMessage(error.response.data.message);
+        }  
+    };
+    
     
     useEffect(() => {
         fetchTodos();
@@ -25,21 +66,62 @@ export default function WorkingWithArraysAsynchronously() {
     return (
         <div id="wd-asynchronous-arrays">
         <h3>Working with Arrays Asynchronously</h3>
+        {errorMessage && (
+            <div 
+                id="wd-todo-error-message" 
+                className="alert alert-danger mb-2 mt-2"
+            >
+                {errorMessage}
+            </div>
+        )}
         <h4>Todos
-            <FaPlusCircle onClick={createTodo} className="text-success float-end fs-3"
-                            id="wd-create-todo" /> </h4>
+            <FaPlusCircle onClick={createTodo} 
+                            className="text-success float-end fs-3"
+                            id="wd-create-todo" />
+            <FaPlusCircle onClick={postTodo}   
+                            className="text-primary float-end fs-3 me-3" 
+                            id="wd-post-todo"   />
+        </h4>
 
         <ul className="list-group">
             {todos.map((todo) => (
             <li key={todo.id} className="list-group-item">
                 <FaTrash onClick={() => removeTodo(todo)}
-                    className="text-danger float-end mt-1" id="wd-remove-todo"/>
+                    className="text-danger float-end mt-1" 
+                    id="wd-remove-todo"/>
+                <TiDelete onClick={() => deleteTodo(todo)} 
+                    className="text-danger float-end me-2 fs-3" 
+                    id="wd-delete-todo" />
 
-                <input type="checkbox" className="form-check-input me-2"
+                <FaPencil onClick={() => editTodo(todo)} 
+                    className="text-primary float-end me-2 mt-1" />
+                <input type="checkbox" defaultChecked={todo.completed} className="form-check-input me-2 float-start"
+                    onChange={(e) => updateTodo({ ...todo, completed: e.target.checked }) } />
+                    
+                    {!todo.editing ? (
+                        <span style={{ textDecoration: todo.completed ? "line-through" : "none" }}> 
+                            {todo.title}
+                        </ span>
+
+                    ) : (
+                        <input className="form-control w-50 float-start" defaultValue={todo.title}
+                        onKeyDown={(e) => {
+                            if (e.key === "Enter") {
+                            saveTodo({ ...todo, editing: false });
+                            }
+                        }}
+                        onChange={(e) =>
+                            updateTodo({ ...todo, title: e.target.value })
+                        }
+                        />
+                    )}         
+
+{/*                 <input type="checkbox" className="form-check-input me-2 float-start"
                     defaultChecked={todo.completed}/>
+
                 <span style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
                 {todo.title}
-                </span>
+                </span> */}
             </li>
             ))}
         </ul> <hr />
--- src/Labs/Lab5/client.ts
+++ src/Labs/Lab5/client.ts
@@ -19,6 +19,16 @@ export const createTodo = async () => {
     return response.data;
 };
 
+export const postTodo = async (todo: any) => {
+    const response = await axios.post(`${TODOS_API}`, todo);
+    return response.data;
+};
+
+export const updateTodo = async (todo: any) => {
+    const response = await axios.put(`${TODOS_API}/${todo.id}`, todo);
+    return response.data;
+};
+
 const TODOS_API = `${REMOTE_SERVER}/lab5/todos`;
 export const fetchTodos = async () => {
     const response = await axios.get(TODOS_API);
@@ -29,4 +39,10 @@ export const removeTodo = async (todo: any) => {
     const response = await axios.get(`${TODOS_API}/${todo.id}/delete`);
     return response.data;
 };
+
+export const deleteTodo = async (todo: any) => {
+    const response = await axios.delete(`${TODOS_API}/${todo.id}`);
+    return response.data;
+};
+
 
